f84947819c75e580758f62479308de2f33ba91f4
Heclypso <heclypso@gmail.com>
2025-03-07 16:09:54 -0300

    #4 feat: desenvolvido sistema de navegação

---
+++ src/scripts/navegation.js
@@ -0,0 +1,28 @@
+const buttons = document.getElementsByTagName('button')
+const log = document.getElementById('log')
+const allys = document.getElementById('allys')
+const skills = document.getElementById('skills')
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(){
+        switch(buttons[i].id) {
+            case ('attack-button'): 
+                allys.classList.remove('battle-menu__infos__allys-is--visible')
+                skills.classList.add('battle-menu__infos__skills-is--visible')
+            break;
+
+            case ('itens-button'): 
+            console.log('Menu de itens foi clicado')
+            break;
+
+            case ('group-button'): 
+                allys.classList.add('battle-menu__infos__allys-is--visible')
+                skills.classList.remove('battle-menu__infos__skills-is--visible')
+            break;
+
+            case ('log-button'): 
+                log.classList.toggle('battle-log-is--visible')
+            break;
+        }
+    })
+}
